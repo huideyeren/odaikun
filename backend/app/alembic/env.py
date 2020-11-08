9dd4b5b84b5c5ac63aa5b3cb0f7f4c69b7030535
huideyeren <takakura.yusuke@gmail.com>
2020-11-09 08:12:19 +0900

    env.pyへのdocstringの追加

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -27,11 +27,18 @@ target_metadata = Base.metadata
 
 
 def get_url():
+    """
+    get_url データベースのURLを環境変数から取得する。
+
+    Returns:
+        (str | None): データベースのURL。無い場合はNone。
+    """
     return os.getenv("DATABASE_URL")
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
+    """
+    Run migrations in 'offline' mode.
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
@@ -53,7 +60,8 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
+    """
+    Run migrations in 'online' mode.
     In this scenario we need to create an Engine
     and associate a connection with the context.
     """
